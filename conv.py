75360d1aa54429af6efcc9fb8a044f91246433b4
Daniil <72657127+danissimmo@users.noreply.github.com>
2023-07-13 01:14:29 +0300

    Update conv.py

--- conv.py
+++ conv.py
@@ -1,10 +1,1 @@
-# importing pandas library
-import pandas as pd
 
-# reading the given csv file
-# and creating dataframe
-account = pd.read_csv("log.txt",
-                      delimiter='	')
-# store dataframe into csv file
-account.to_csv('logi.csv',
-               index=None)
